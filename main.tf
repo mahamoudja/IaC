b962b2eb37d4c387a4a6ee2a35a65fb49a561afd
mahamoud jama <maxaamoud@gmail.com>
2024-06-06 00:10:22 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure location where resources will be created"
+  type        = string
+  default     = "East US"
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+  default     = "aks-vnet"
+}
+
+variable "vnet_address_space" {
+  description = "The address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+  default     = "aks-subnet"
+}
+
+variable "subnet_address_prefix" {
+  description = "The address prefix for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
